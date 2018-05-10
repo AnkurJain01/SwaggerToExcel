34b418bd4bf16b9f29c8bdfd83513571548e3de0
AnkurJain01 <ankur93jain@gmail.com>
2018-05-10 23:06:55 +0530

    main class

--- src/main/java/excel/json/MainClass.java
+++ src/main/java/excel/json/MainClass.java
@@ -2,9 +2,10 @@ package excel.json;
 
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
+		JsonToExcel.exportswaggerUrlToExcel("http://35.169.81.105:8080/AMP/v2/api-docs");
 	}
 
 }
